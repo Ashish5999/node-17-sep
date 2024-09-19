af3552a5177467258d0f9c5af6e13e468508e9cf
Ashish5999 <chaudharyashish3101@gmail.com>
2024-09-19 14:20:41 +0530

    updation..

---
+++ controllers/studentcontroller.js
@@ -0,0 +1,36 @@
+const Student = require('../models/Student');
+
+async function addStudent(req,res) {
+    try{
+        console.log(req.body,'req.body')
+        let student = new Student(req.body);
+        await student.save();
+        console.log("data saved sucessfully....")
+        res.end("data added...")
+    }catch(err){
+        console.log(err)
+    }
+}
+const getStudents =async function (req,res){
+    try{
+        let students = await Student.find({});
+        console.log(students)
+        res.send(students)
+    }catch(err){
+        console.log(err)
+    }
+}
+async function getStudentsById(req,res){
+    try{
+        let students=await Student.find({});
+        console.log(students,'students');
+        res.send(students)
+    }catch(err){
+
+    }
+}
+module.exports= {
+    addStudent,
+    getStudents,
+    getStudentsById
+}
